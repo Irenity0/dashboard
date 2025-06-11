a0793e21930897a1a7355e5837f39df62aa8f42c
Afra Anjum <lilithkard77@gmail.com>
2025-06-11 13:42:59 +0600

    neatly formated all day dates for event table

--- src/pages/EventsTable.tsx
+++ src/pages/EventsTable.tsx
@@ -1,19 +1,23 @@
-import { getEventColorClasses } from "@/components"
-import { useAuth } from "@/hooks/useAuth"
-import useEvents from "@/hooks/useEvents"
+import { getEventColorClasses } from "@/components";
+import { useAuth } from "@/hooks/useAuth";
+import useEvents from "@/hooks/useEvents";
 
 const EventsTable = () => {
-  const { user } = useAuth()
-  const { data: events, isLoading: loading, isError } = useEvents(user!.email!)
+  const { user } = useAuth();
+  const { data: events, isLoading: loading, isError } = useEvents(user!.email!);
 
   return (
     <div className="w-full max-w-4xl mx-auto mt-4">
       {loading ? (
         <p className="text-sm text-center">Loading events...</p>
       ) : isError ? (
-        <p className="text-sm text-red-500 text-center">⚠️ Failed to load events. Please try again later.</p>
+        <p className="text-sm text-red-500 text-center">
+          ⚠️ Failed to load events. Please try again later.
+        </p>
       ) : events!.length === 0 ? (
-        <p className="text-center text-zinc-400">You haven’t added any tasks yet {">.<"}</p>
+        <p className="text-center text-zinc-400">
+          You haven’t added any tasks yet {">.<"}
+        </p>
       ) : (
         <table className="w-full border text-sm border-collapse overflow-hidden">
           <thead className="text-left bg-zinc-900">
@@ -32,11 +36,24 @@ const EventsTable = () => {
               >
                 <td className="border px-2 py-1">{event.title}</td>
                 <td className="border px-2 py-1">
-                  {event.allDay ? "All day" : new Date(event.start).toLocaleString()}
+                  {event.allDay
+                    ? `${new Date(event.start).toLocaleDateString("en-GB", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}`
+                    : new Date(event.start).toLocaleString()}
                 </td>
                 <td className="border px-2 py-1">
-                  {event.allDay ? "All day" : new Date(event.end).toLocaleString()}
+                  {event.allDay
+                    ? `${new Date(event.end).toLocaleDateString("en-GB", {
+                        day: "numeric",
+                        month: "long",
+                        year: "numeric",
+                      })}`
+                    : new Date(event.end).toLocaleString()}
                 </td>
+
                 <td className="border px-2 py-1">{event.location}</td>
               </tr>
             ))}
@@ -44,7 +61,7 @@ const EventsTable = () => {
         </table>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default EventsTable
+export default EventsTable;
